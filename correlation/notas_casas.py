a366cf38ad2d6a11b64d5c0ebe597362cc43620d
Denys Malta <denysfmalta@hotmail.com>
2024-05-15 13:40:36 -0300

    Corrigindo um bug

--- correlation/notas_casas.py
+++ correlation/notas_casas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_excel('dataset/regression.xls', sheet_name="notas_casas.py")
+df = pd.read_excel('dataset/regression.xls', sheet_name="notas_casas")
 
 correlation_matrix = df.corr()
 
